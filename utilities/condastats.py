46ac636ba52d1a166c24ddf12e81c20af9b3b87f
Kevin Paul <kpaul@ucar.edu>
2022-02-01 16:17:19 -0700

    Refactor

--- utilities/condastats.py
+++ utilities/condastats.py
@@ -1,27 +1,87 @@
-"""
-This file contains a function to read data from a public AWS S3 store containing hourly
-Conda package download data.
-"""
+#!/usr/bin/env python
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from pathlib import Path
 
 import dask.dataframe as dd
+import pandas as pd
+
+from common import update_stats, read_stats_file
+from xdevrepos import read_repos_file, get_packages
 
 
-def get_conda_stats_for_month(packages=[], year=2022, month=1):
+STATS_FILE = Path(__file__).with_suffix('.csv')
+
+
+def get_conda_stats(packages=[], year=None, month=None):
+    """
+    Read Conda download statistics from AWS S3
+
+    This function readS data from a public AWS S3 store containing hourly
+    Conda package download data.
+    """
     if not packages:
         return None
 
-    s3uri = f's3://anaconda-package-data/conda/hourly/{year:04d}/{month:02d}/{year:04d}-{month:02d}-*.parquet'
-    df = dd.read_parquet(
-        s3uri,
-        columns=['time', 'data_source', 'pkg_name', 'counts'],
-        storage_options={'anon': True},
-        engine='pyarrow',
-    )
+    if isinstance(year, (list, tuple)):
+        years = [f'{y:04d}' for y in year]
+    elif isinstance(year, int):
+        years = [f'{year:04d}']
+    elif isinstance(year, str):
+        years = [year]
+    elif year is None:
+        years = ['*']
+    else:
+        years = []
 
-    df = df.loc[df.pkg_name.isin(['intake-esm', 'jupyter-forward', 'ncar-jobqueue'])].compute()
-    df.pkg_name = df.pkg_name.cat.remove_unused_categories()
-    df = df.groupby([df.time.dt.to_period('M'), df.pkg_name]).sum().reset_index()
-    df = df.pivot(index='time', columns='pkg_name', values='counts')
-    df.index.name = 'month'
+    if isinstance(month, (list, tuple)):
+        months = [f'{m:02d}' for m in month]
+    elif isinstance(month, int):
+        months = [f'{month:02d}']
+    elif isinstance(month, str):
+        months = [month]
+    elif month is None:
+        months = ['*']
+    else:
+        months = []
 
-    return df
+    dfs = []
+    for yr in years:
+        for mo in months:
+            s3uri = f's3://anaconda-package-data/conda/hourly/{yr}/{mo}/*-*-*.parquet'
+            df = dd.read_parquet(
+                s3uri,
+                storage_options={'anon': True},
+                engine='pyarrow',
+            )
+            if 'pkg_name' not in df:
+                continue
+
+            df = df[['time', 'data_source', 'pkg_name', 'counts']]
+            df = df.loc[df.pkg_name.isin(packages)].compute()
+            df.pkg_name = df.pkg_name.cat.remove_unused_categories()
+            df = df.groupby([df.time.dt.to_period('M'), df.pkg_name]).sum().reset_index()
+            df = df.pivot(index='time', columns='pkg_name', values='counts')
+            df.index.name = 'month'
+            dfs.append(df)
+
+    if dfs:
+        return pd.concat(dfs).fillna(0).astype('int64')
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    repos = read_repos_file()
+    packages = get_packages(repos, kind='conda')
+
+    STATS_FILE = Path(__file__).with_suffix('.csv')
+    if STATS_FILE.exists():
+        print('Updating Conda stats...')
+        df = read_stats_file(STATS_FILE)
+        df = update_stats(df, packages=packages, method=get_conda_stats)
+    else:
+        print('Downloading all Conda stats...')
+        df = get_conda_stats(packages=packages)
+
+    df.to_csv(STATS_FILE)
--- utilities/pypistats.py
+++ utilities/pypistats.py
@@ -1,24 +1,36 @@
-"""
-This file contains a simple function to read data from Google BigQuery (i.e., via SQL).
-To access Google BigQuery, you need a Google Cloud Project with the BigQuery API
-enabled.  (Note, you cannot do this with your NCAR Google account!)  Once you have
-a BigQuery-enabled GC project, you can download credentials to your laptop/machine as
-a JSON, and you can set the environment variable:
-
-GOOGLE_APPLICATION_CREDENTIALS="path/to/credentials.json"
-
-Without the credentials file AND this environment variable set, this funciton will
-fail with authentication errors.
-"""
-
+#!/usr/bin/env python
 from calendar import monthrange
+from pathlib import Path
 
 from google.cloud import bigquery
+import pandas as pd
+
+from common import update_stats, read_stats_file
+from xdevrepos import read_repos_file, get_packages
 
 
-def get_pypi_stats_for_month(packages=[], year=2022, month=1):
+def get_pypi_stats(packages=[], year=None, month=None):
+    """
+    Read PyPI download statistics from Google BigQuery
+    """
     if not packages:
         return None
+
+    if isinstance(year, (list, tuple)):
+        year1, year2 = min(year), max(year)
+    elif year is None:
+        year1, year2 = 2018, 2030
+    else:
+        year1 = year2 = year
+
+    if isinstance(month, (list, tuple)):
+        month1, month2 = min(month), max(month)
+    elif month is None:
+        month1, month2 = 1, 12
+    else:
+        month1 = month2 = month
+
+    day1, day2 = 1, monthrange(year2, month2)[1]
 
     client = bigquery.Client()
 
@@ -36,15 +48,32 @@ def get_pypi_stats_for_month(packages=[], year=2022, month=1):
     FROM `bigquery-public-data.pypi.file_downloads`
     WHERE
         file.project in ({pkg_string})
-        AND DATE(timestamp) >= '{year:04d}-{month:02d}-01'
-        AND DATE(timestamp) <= '{year:04d}-{month:02d}-{monthrange(year, month)[1]:02d}'
+        AND DATE(timestamp) >= '{year1:04d}-{month1:02d}-{day1:02d}'
+        AND DATE(timestamp) <= '{year2:04d}-{month2:02d}-{day2:02d}'
     GROUP BY `month`
     ORDER BY `month` ASC
     """
 
     df = client.query(sql).to_dataframe()
-    df.rename(columns={f'p{i}':p for i,p in enumerate(packages)}, inplace=True)
-    df = df.set_index('month')
+    df = df.rename(columns={f'p{i}':p for i,p in enumerate(packages)})
+    df['month'] = pd.to_datetime(df.month).dt.to_period('M')
     df.columns.name = 'pkg_name'
+    df = df.set_index('month')
 
     return df
+
+
+if __name__ == '__main__':
+    repos = read_repos_file()
+    packages = get_packages(repos, kind='pypi')
+
+    STATS_FILE = Path(__file__).with_suffix('.csv')
+    if STATS_FILE.exists():
+        print('Updating PyPI stats...')
+        df = read_stats_file(STATS_FILE)
+        df = update_stats(df, packages=packages, method=get_pypi_stats)
+    else:
+        print('Downloading all PyPI stats...')
+        df = get_pypi_stats(packages=packages)
+
+    df.to_csv(STATS_FILE)
