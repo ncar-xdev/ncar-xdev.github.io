42965542039ab04e9fb6f566a68f6d998b9bafdf
Kevin Paul <kpaul@ucar.edu>
2022-02-08 11:27:38 -0700

    Formatting

--- cli/mkmetrics.py
+++ cli/mkmetrics.py
@@ -21,7 +21,12 @@ pkgs = set(df_pypi.columns).union(df_conda.columns)
 pkg_dfs = {}
 for pkg in pkgs:
     df = pd.DataFrame()
-    df.index = pd.period_range(start='2019-01', periods=0, freq='M', name='month')
+    df.index = pd.period_range(
+        start='2019-01',
+        periods=0,
+        freq='M',
+        name='month',
+    )
     include_df = False
     if pkg in df_pypi and df_pypi[pkg].sum() > 0:
         df['PyPI'] = df_pypi[pkg]
@@ -72,14 +77,25 @@ for pkg in pkg_dfs:
         y_range=(0, df.sum(axis=1).max()*1.1),
         sizing_mode='scale_width',
     )
-    p.varea_stack(stackers=names, x='month', color=Blues4[:len(names)], legend_label=names, source=df)
-    p.vline_stack(stackers=names, x='month', color=Blues4[:len(names)], source=df)
+    p.varea_stack(
+        stackers=names,
+        x='month',
+        color=Blues4[:len(names)],
+        legend_label=names,
+        source=df,
+    )
+    p.vline_stack(
+        stackers=names,
+        x='month',
+        color=Blues4[:len(names)],
+        source=df,
+    )
     p.xaxis.formatter = dtformatter
     p.legend.location = 'top_left'
     save(p)
 
 
-metrics_md = f"""# Metrics
+metrics_md = """# Metrics
 
 Below are some of the metrics of things that Xdev does.
 
